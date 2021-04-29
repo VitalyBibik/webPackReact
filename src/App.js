a6999850fa2ee073865c41ecc01d0a5d4f2b6537
Vitaly Bibik <wital2010@gmail.com>
2021-04-29 16:46:07 +0300

    task 3 is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './style.css';
+import dog from './asserts/images/dog.jpg'
 
 const App = ({ title }) =>
-    <h1 className={styles.title}>{title}</h1>;
+    <>
+    <h1 className={styles.title}>{title}</h1>
+        <img src={dog} alt="SuperDog"/>
+    </>
 
 export default App;
