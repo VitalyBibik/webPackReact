6a362116c90f2b4d469f0838909b4b2ebcb6a3d3
Vitaly Bibik <wital2010@gmail.com>
2021-04-29 15:39:51 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React с Webpack и Babel';
+
+ReactDOM.render(
+    <h1>{title}</h1>,
+    document.getElementById('app')
+);
