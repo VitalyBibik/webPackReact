90327d665d5fad26e7569af743754aa8200be331
Vitaly Bibik <wital2010@gmail.com>
2021-04-29 16:46:07 +0300

    task 3 is done

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,12 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test:/\.(jpg|png)$/,
+                use: {
+                    loader: "url-loader"
+                }
             }
         ],
     },
