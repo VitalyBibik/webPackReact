17c359c2092bb691acd83bc97c8c54948cde3ece
Vitaly Bibik <wital2010@gmail.com>
2021-05-10 15:17:02 +0300

    Add scss in webpack

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './style.css';
 import dog from './asserts/images/dog.jpg';
+import {Button} from "./Button";
 
 const App = ({ title }) => (
   <>
     <h1 className={styles.title}>{title}</h1>
-    <img src={dog} alt="SuperDog" />
+    <Button/>
   </>
 );
 
